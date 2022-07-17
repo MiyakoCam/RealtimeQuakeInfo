d13938d2dc256a4d8cd8cbf5b49bcc3ce28252be
MiyakoCam <kotarokotaro123456@gmail.com>
2022-07-17 17:22:14 +0900

    Add files via upload

--- test.c
+++ test.c
@@ -256,3 +256,777 @@ T.2　Y.2
 NOW.2　TOTAL.18
 地震ライブネットワーク　EWRS共有局が管理しております。　荒らしは犯罪です。おやめください。
 
+地震観測・監視界隈wiki
+TopPage【Japanese】
+管理：地震ライブネットワーク(代表：EWRS・WEDR・NBI KM葛飾)
+
+[ ホーム | 新規 | 編集 | 添付 ]
+ 
+一覧 最終更新 差分 バックアップ 凍結 複製 名前変更 ヘルプ
+サイト内検索
+ 
+Top > RealtimeQuakeInfo
+ 9.1ms 
+RealtimeQuakeInfo
+Last-modified: 2022-07-09 (土) 16:41:09
+RealtimeQuakeInfoは、MiyakojimaCamが開発・運営する、緊急地震速報をWeb上で受信できるようにしたアプリです。
+公開ページ：http://nanka.html.xdomain.jp/html/RealtimeQuakeInfo/
+
+
+概要 
+RealtimeQuakeInfoは、Web上で緊急地震速報を受信し、予想円を地図上に描画したり、最大震度、到達予想時間などを画面上に表示します。
+
+
+特徴 
+Webブラウザ上で実行可能
+大きく分かりやすい震度表示
+自分好みの配色に設定可能
+到達予想時間分かる
+リプレイ機能搭載
+
+お借りしているAPI等 
+強震モニタ by Wolf20482様 - 緊急地震速報を受信するため
+Yahoo地震情報 by Yahoo様 - 予報円を受信するため
+WorldTimeAPI - 時刻を取得するため
+
+技術仕様 
+URLに「?replay_daytime=20220709163752」などと、年月日時分秒の順に14桁の数字を入れると、その時間からリプレイを開始します。（例では、2022年07月09日16時37分52秒からリプレイが開始されます。）
+
+
+
+ 
+地震監視界隈
+Top
+地震監視者
+地震観測者
+地震情報提供者
+地震情報共有団体
+地震監視配信団体
+地震監視とは
+地震観測とは
+情報提供とは
+観測と監視の違い
+監視ソフト一覧
+地震ライブネットワーク
+地震観測点情報
+
+最新の20件
+2022-07-12
+地震観測者
+関東地方
+九州・沖縄地方
+中部地方
+東北地方
+近畿地方
+2022-07-09
+MiyakojimaCam
+RealtimeQuakeInfo
+RecentCreated
+監視ソフト一覧
+2022-07-02
+KagoshimaCam
+2022-07-01
+編集希望まとめ
+2022-06-25
+JSnetwork
+2022-06-19
+EWRS共有局
+近くの畑のおネギ
+KuriharaCam
+FrontPage
+日本語
+RecentDeleted
+MenuBar
+〔人気/今日人気〕〔最新〕
+T.2　Y.2
+NOW.2　TOTAL.18
+地震ライブネットワーク　EWRS共有局が管理しております。　荒らしは犯罪です。おやめください。
+
+地震観測・監視界隈wiki
+TopPage【Japanese】
+管理：地震ライブネットワーク(代表：EWRS・WEDR・NBI KM葛飾)
+
+[ ホーム | 新規 | 編集 | 添付 ]
+ 
+一覧 最終更新 差分 バックアップ 凍結 複製 名前変更 ヘルプ
+サイト内検索
+ 
+Top > RealtimeQuakeInfo
+ 9.1ms 
+RealtimeQuakeInfo
+Last-modified: 2022-07-09 (土) 16:41:09
+RealtimeQuakeInfoは、MiyakojimaCamが開発・運営する、緊急地震速報をWeb上で受信できるようにしたアプリです。
+公開ページ：http://nanka.html.xdomain.jp/html/RealtimeQuakeInfo/
+
+
+概要 
+RealtimeQuakeInfoは、Web上で緊急地震速報を受信し、予想円を地図上に描画したり、最大震度、到達予想時間などを画面上に表示します。
+
+
+特徴 
+Webブラウザ上で実行可能
+大きく分かりやすい震度表示
+自分好みの配色に設定可能
+到達予想時間分かる
+リプレイ機能搭載
+
+お借りしているAPI等 
+強震モニタ by Wolf20482様 - 緊急地震速報を受信するため
+Yahoo地震情報 by Yahoo様 - 予報円を受信するため
+WorldTimeAPI - 時刻を取得するため
+
+技術仕様 
+URLに「?replay_daytime=20220709163752」などと、年月日時分秒の順に14桁の数字を入れると、その時間からリプレイを開始します。（例では、2022年07月09日16時37分52秒からリプレイが開始されます。）
+
+
+
+ 
+地震監視界隈
+Top
+地震監視者
+地震観測者
+地震情報提供者
+地震情報共有団体
+地震監視配信団体
+地震監視とは
+地震観測とは
+情報提供とは
+観測と監視の違い
+監視ソフト一覧
+地震ライブネットワーク
+地震観測点情報
+
+最新の20件
+2022-07-12
+地震観測者
+関東地方
+九州・沖縄地方
+中部地方
+東北地方
+近畿地方
+2022-07-09
+MiyakojimaCam
+RealtimeQuakeInfo
+RecentCreated
+監視ソフト一覧
+2022-07-02
+KagoshimaCam
+2022-07-01
+編集希望まとめ
+2022-06-25
+JSnetwork
+2022-06-19
+EWRS共有局
+近くの畑のおネギ
+KuriharaCam
+FrontPage
+日本語
+RecentDeleted
+MenuBar
+〔人気/今日人気〕〔最新〕
+T.2　Y.2
+NOW.2　TOTAL.18
+地震ライブネットワーク　EWRS共有局が管理しております。　荒らしは犯罪です。おやめください。
+
+地震観測・監視界隈wiki
+TopPage【Japanese】
+管理：地震ライブネットワーク(代表：EWRS・WEDR・NBI KM葛飾)
+
+[ ホーム | 新規 | 編集 | 添付 ]
+ 
+一覧 最終更新 差分 バックアップ 凍結 複製 名前変更 ヘルプ
+サイト内検索
+ 
+Top > RealtimeQuakeInfo
+ 9.1ms 
+RealtimeQuakeInfo
+Last-modified: 2022-07-09 (土) 16:41:09
+RealtimeQuakeInfoは、MiyakojimaCamが開発・運営する、緊急地震速報をWeb上で受信できるようにしたアプリです。
+公開ページ：http://nanka.html.xdomain.jp/html/RealtimeQuakeInfo/
+
+
+概要 
+RealtimeQuakeInfoは、Web上で緊急地震速報を受信し、予想円を地図上に描画したり、最大震度、到達予想時間などを画面上に表示します。
+
+
+特徴 
+Webブラウザ上で実行可能
+大きく分かりやすい震度表示
+自分好みの配色に設定可能
+到達予想時間分かる
+リプレイ機能搭載
+
+お借りしているAPI等 
+強震モニタ by Wolf20482様 - 緊急地震速報を受信するため
+Yahoo地震情報 by Yahoo様 - 予報円を受信するため
+WorldTimeAPI - 時刻を取得するため
+
+技術仕様 
+URLに「?replay_daytime=20220709163752」などと、年月日時分秒の順に14桁の数字を入れると、その時間からリプレイを開始します。（例では、2022年07月09日16時37分52秒からリプレイが開始されます。）
+
+
+
+ 
+地震監視界隈
+Top
+地震監視者
+地震観測者
+地震情報提供者
+地震情報共有団体
+地震監視配信団体
+地震監視とは
+地震観測とは
+情報提供とは
+観測と監視の違い
+監視ソフト一覧
+地震ライブネットワーク
+地震観測点情報
+
+最新の20件
+2022-07-12
+地震観測者
+関東地方
+九州・沖縄地方
+中部地方
+東北地方
+近畿地方
+2022-07-09
+MiyakojimaCam
+RealtimeQuakeInfo
+RecentCreated
+監視ソフト一覧
+2022-07-02
+KagoshimaCam
+2022-07-01
+編集希望まとめ
+2022-06-25
+JSnetwork
+2022-06-19
+EWRS共有局
+近くの畑のおネギ
+KuriharaCam
+FrontPage
+日本語
+RecentDeleted
+MenuBar
+〔人気/今日人気〕〔最新〕
+T.2　Y.2
+NOW.2　TOTAL.18
+地震ライブネットワーク　EWRS共有局が管理しております。　荒らしは犯罪です。おやめください。
+
+地震観測・監視界隈wiki
+TopPage【Japanese】
+管理：地震ライブネットワーク(代表：EWRS・WEDR・NBI KM葛飾)
+
+[ ホーム | 新規 | 編集 | 添付 ]
+ 
+一覧 最終更新 差分 バックアップ 凍結 複製 名前変更 ヘルプ
+サイト内検索
+ 
+Top > RealtimeQuakeInfo
+ 9.1ms 
+RealtimeQuakeInfo
+Last-modified: 2022-07-09 (土) 16:41:09
+RealtimeQuakeInfoは、MiyakojimaCamが開発・運営する、緊急地震速報をWeb上で受信できるようにしたアプリです。
+公開ページ：http://nanka.html.xdomain.jp/html/RealtimeQuakeInfo/
+
+
+概要 
+RealtimeQuakeInfoは、Web上で緊急地震速報を受信し、予想円を地図上に描画したり、最大震度、到達予想時間などを画面上に表示します。
+
+
+特徴 
+Webブラウザ上で実行可能
+大きく分かりやすい震度表示
+自分好みの配色に設定可能
+到達予想時間分かる
+リプレイ機能搭載
+
+お借りしているAPI等 
+強震モニタ by Wolf20482様 - 緊急地震速報を受信するため
+Yahoo地震情報 by Yahoo様 - 予報円を受信するため
+WorldTimeAPI - 時刻を取得するため
+
+技術仕様 
+URLに「?replay_daytime=20220709163752」などと、年月日時分秒の順に14桁の数字を入れると、その時間からリプレイを開始します。（例では、2022年07月09日16時37分52秒からリプレイが開始されます。）
+
+
+
+ 
+地震監視界隈
+Top
+地震監視者
+地震観測者
+地震情報提供者
+地震情報共有団体
+地震監視配信団体
+地震監視とは
+地震観測とは
+情報提供とは
+観測と監視の違い
+監視ソフト一覧
+地震ライブネットワーク
+地震観測点情報
+
+最新の20件
+2022-07-12
+地震観測者
+関東地方
+九州・沖縄地方
+中部地方
+東北地方
+近畿地方
+2022-07-09
+MiyakojimaCam
+RealtimeQuakeInfo
+RecentCreated
+監視ソフト一覧
+2022-07-02
+KagoshimaCam
+2022-07-01
+編集希望まとめ
+2022-06-25
+JSnetwork
+2022-06-19
+EWRS共有局
+近くの畑のおネギ
+KuriharaCam
+FrontPage
+日本語
+RecentDeleted
+MenuBar
+〔人気/今日人気〕〔最新〕
+T.2　Y.2
+NOW.2　TOTAL.18
+地震ライブネットワーク　EWRS共有局が管理しております。　荒らしは犯罪です。おやめください。
+
+地震観測・監視界隈wiki
+TopPage【Japanese】
+管理：地震ライブネットワーク(代表：EWRS・WEDR・NBI KM葛飾)
+
+[ ホーム | 新規 | 編集 | 添付 ]
+ 
+一覧 最終更新 差分 バックアップ 凍結 複製 名前変更 ヘルプ
+サイト内検索
+ 
+Top > RealtimeQuakeInfo
+ 9.1ms 
+RealtimeQuakeInfo
+Last-modified: 2022-07-09 (土) 16:41:09
+RealtimeQuakeInfoは、MiyakojimaCamが開発・運営する、緊急地震速報をWeb上で受信できるようにしたアプリです。
+公開ページ：http://nanka.html.xdomain.jp/html/RealtimeQuakeInfo/
+
+
+概要 
+RealtimeQuakeInfoは、Web上で緊急地震速報を受信し、予想円を地図上に描画したり、最大震度、到達予想時間などを画面上に表示します。
+
+
+特徴 
+Webブラウザ上で実行可能
+大きく分かりやすい震度表示
+自分好みの配色に設定可能
+到達予想時間分かる
+リプレイ機能搭載
+
+お借りしているAPI等 
+強震モニタ by Wolf20482様 - 緊急地震速報を受信するため
+Yahoo地震情報 by Yahoo様 - 予報円を受信するため
+WorldTimeAPI - 時刻を取得するため
+
+技術仕様 
+URLに「?replay_daytime=20220709163752」などと、年月日時分秒の順に14桁の数字を入れると、その時間からリプレイを開始します。（例では、2022年07月09日16時37分52秒からリプレイが開始されます。）
+
+
+
+ 
+地震監視界隈
+Top
+地震監視者
+地震観測者
+地震情報提供者
+地震情報共有団体
+地震監視配信団体
+地震監視とは
+地震観測とは
+情報提供とは
+観測と監視の違い
+監視ソフト一覧
+地震ライブネットワーク
+地震観測点情報
+
+最新の20件
+2022-07-12
+地震観測者
+関東地方
+九州・沖縄地方
+中部地方
+東北地方
+近畿地方
+2022-07-09
+MiyakojimaCam
+RealtimeQuakeInfo
+RecentCreated
+監視ソフト一覧
+2022-07-02
+KagoshimaCam
+2022-07-01
+編集希望まとめ
+2022-06-25
+JSnetwork
+2022-06-19
+EWRS共有局
+近くの畑のおネギ
+KuriharaCam
+FrontPage
+日本語
+RecentDeleted
+MenuBar
+〔人気/今日人気〕〔最新〕
+T.2　Y.2
+NOW.2　TOTAL.18
+地震ライブネットワーク　EWRS共有局が管理しております。　荒らしは犯罪です。おやめください。
+
+地震観測・監視界隈wiki
+TopPage【Japanese】
+管理：地震ライブネットワーク(代表：EWRS・WEDR・NBI KM葛飾)
+
+[ ホーム | 新規 | 編集 | 添付 ]
+ 
+一覧 最終更新 差分 バックアップ 凍結 複製 名前変更 ヘルプ
+サイト内検索
+ 
+Top > RealtimeQuakeInfo
+ 9.1ms 
+RealtimeQuakeInfo
+Last-modified: 2022-07-09 (土) 16:41:09
+RealtimeQuakeInfoは、MiyakojimaCamが開発・運営する、緊急地震速報をWeb上で受信できるようにしたアプリです。
+公開ページ：http://nanka.html.xdomain.jp/html/RealtimeQuakeInfo/
+
+
+概要 
+RealtimeQuakeInfoは、Web上で緊急地震速報を受信し、予想円を地図上に描画したり、最大震度、到達予想時間などを画面上に表示します。
+
+
+特徴 
+Webブラウザ上で実行可能
+大きく分かりやすい震度表示
+自分好みの配色に設定可能
+到達予想時間分かる
+リプレイ機能搭載
+
+お借りしているAPI等 
+強震モニタ by Wolf20482様 - 緊急地震速報を受信するため
+Yahoo地震情報 by Yahoo様 - 予報円を受信するため
+WorldTimeAPI - 時刻を取得するため
+
+技術仕様 
+URLに「?replay_daytime=20220709163752」などと、年月日時分秒の順に14桁の数字を入れると、その時間からリプレイを開始します。（例では、2022年07月09日16時37分52秒からリプレイが開始されます。）
+
+
+
+ 
+地震監視界隈
+Top
+地震監視者
+地震観測者
+地震情報提供者
+地震情報共有団体
+地震監視配信団体
+地震監視とは
+地震観測とは
+情報提供とは
+観測と監視の違い
+監視ソフト一覧
+地震ライブネットワーク
+地震観測点情報
+
+最新の20件
+2022-07-12
+地震観測者
+関東地方
+九州・沖縄地方
+中部地方
+東北地方
+近畿地方
+2022-07-09
+MiyakojimaCam
+RealtimeQuakeInfo
+RecentCreated
+監視ソフト一覧
+2022-07-02
+KagoshimaCam
+2022-07-01
+編集希望まとめ
+2022-06-25
+JSnetwork
+2022-06-19
+EWRS共有局
+近くの畑のおネギ
+KuriharaCam
+FrontPage
+日本語
+RecentDeleted
+MenuBar
+〔人気/今日人気〕〔最新〕
+T.2　Y.2
+NOW.2　TOTAL.18
+地震ライブネットワーク　EWRS共有局が管理しております。　荒らしは犯罪です。おやめください。
+
+地震観測・監視界隈wiki
+TopPage【Japanese】
+管理：地震ライブネットワーク(代表：EWRS・WEDR・NBI KM葛飾)
+
+[ ホーム | 新規 | 編集 | 添付 ]
+ 
+一覧 最終更新 差分 バックアップ 凍結 複製 名前変更 ヘルプ
+サイト内検索
+ 
+Top > RealtimeQuakeInfo
+ 9.1ms 
+RealtimeQuakeInfo
+Last-modified: 2022-07-09 (土) 16:41:09
+RealtimeQuakeInfoは、MiyakojimaCamが開発・運営する、緊急地震速報をWeb上で受信できるようにしたアプリです。
+公開ページ：http://nanka.html.xdomain.jp/html/RealtimeQuakeInfo/
+
+
+概要 
+RealtimeQuakeInfoは、Web上で緊急地震速報を受信し、予想円を地図上に描画したり、最大震度、到達予想時間などを画面上に表示します。
+
+
+特徴 
+Webブラウザ上で実行可能
+大きく分かりやすい震度表示
+自分好みの配色に設定可能
+到達予想時間分かる
+リプレイ機能搭載
+
+お借りしているAPI等 
+強震モニタ by Wolf20482様 - 緊急地震速報を受信するため
+Yahoo地震情報 by Yahoo様 - 予報円を受信するため
+WorldTimeAPI - 時刻を取得するため
+
+技術仕様 
+URLに「?replay_daytime=20220709163752」などと、年月日時分秒の順に14桁の数字を入れると、その時間からリプレイを開始します。（例では、2022年07月09日16時37分52秒からリプレイが開始されます。）
+
+
+
+ 
+地震監視界隈
+Top
+地震監視者
+地震観測者
+地震情報提供者
+地震情報共有団体
+地震監視配信団体
+地震監視とは
+地震観測とは
+情報提供とは
+観測と監視の違い
+監視ソフト一覧
+地震ライブネットワーク
+地震観測点情報
+
+最新の20件
+2022-07-12
+地震観測者
+関東地方
+九州・沖縄地方
+中部地方
+東北地方
+近畿地方
+2022-07-09
+MiyakojimaCam
+RealtimeQuakeInfo
+RecentCreated
+監視ソフト一覧
+2022-07-02
+KagoshimaCam
+2022-07-01
+編集希望まとめ
+2022-06-25
+JSnetwork
+2022-06-19
+EWRS共有局
+近くの畑のおネギ
+KuriharaCam
+FrontPage
+日本語
+RecentDeleted
+MenuBar
+〔人気/今日人気〕〔最新〕
+T.2　Y.2
+NOW.2　TOTAL.18
+地震ライブネットワーク　EWRS共有局が管理しております。　荒らしは犯罪です。おやめください。
+
+地震観測・監視界隈wiki
+TopPage【Japanese】
+管理：地震ライブネットワーク(代表：EWRS・WEDR・NBI KM葛飾)
+
+[ ホーム | 新規 | 編集 | 添付 ]
+ 
+一覧 最終更新 差分 バックアップ 凍結 複製 名前変更 ヘルプ
+サイト内検索
+ 
+Top > RealtimeQuakeInfo
+ 9.1ms 
+RealtimeQuakeInfo
+Last-modified: 2022-07-09 (土) 16:41:09
+RealtimeQuakeInfoは、MiyakojimaCamが開発・運営する、緊急地震速報をWeb上で受信できるようにしたアプリです。
+公開ページ：http://nanka.html.xdomain.jp/html/RealtimeQuakeInfo/
+
+
+概要 
+RealtimeQuakeInfoは、Web上で緊急地震速報を受信し、予想円を地図上に描画したり、最大震度、到達予想時間などを画面上に表示します。
+
+
+特徴 
+Webブラウザ上で実行可能
+大きく分かりやすい震度表示
+自分好みの配色に設定可能
+到達予想時間分かる
+リプレイ機能搭載
+
+お借りしているAPI等 
+強震モニタ by Wolf20482様 - 緊急地震速報を受信するため
+Yahoo地震情報 by Yahoo様 - 予報円を受信するため
+WorldTimeAPI - 時刻を取得するため
+
+技術仕様 
+URLに「?replay_daytime=20220709163752」などと、年月日時分秒の順に14桁の数字を入れると、その時間からリプレイを開始します。（例では、2022年07月09日16時37分52秒からリプレイが開始されます。）
+
+
+
+ 
+地震監視界隈
+Top
+地震監視者
+地震観測者
+地震情報提供者
+地震情報共有団体
+地震監視配信団体
+地震監視とは
+地震観測とは
+情報提供とは
+観測と監視の違い
+監視ソフト一覧
+地震ライブネットワーク
+地震観測点情報
+
+最新の20件
+2022-07-12
+地震観測者
+関東地方
+九州・沖縄地方
+中部地方
+東北地方
+近畿地方
+2022-07-09
+MiyakojimaCam
+RealtimeQuakeInfo
+RecentCreated
+監視ソフト一覧
+2022-07-02
+KagoshimaCam
+2022-07-01
+編集希望まとめ
+2022-06-25
+JSnetwork
+2022-06-19
+EWRS共有局
+近くの畑のおネギ
+KuriharaCam
+FrontPage
+日本語
+RecentDeleted
+MenuBar
+〔人気/今日人気〕〔最新〕
+T.2　Y.2
+NOW.2　TOTAL.18
+地震ライブネットワーク　EWRS共有局が管理しております。　荒らしは犯罪です。おやめください。
+
+地震観測・監視界隈wiki
+TopPage【Japanese】
+管理：地震ライブネットワーク(代表：EWRS・WEDR・NBI KM葛飾)
+
+[ ホーム | 新規 | 編集 | 添付 ]
+ 
+一覧 最終更新 差分 バックアップ 凍結 複製 名前変更 ヘルプ
+サイト内検索
+ 
+Top > RealtimeQuakeInfo
+ 9.1ms 
+RealtimeQuakeInfo
+Last-modified: 2022-07-09 (土) 16:41:09
+RealtimeQuakeInfoは、MiyakojimaCamが開発・運営する、緊急地震速報をWeb上で受信できるようにしたアプリです。
+公開ページ：http://nanka.html.xdomain.jp/html/RealtimeQuakeInfo/
+
+
+概要 
+RealtimeQuakeInfoは、Web上で緊急地震速報を受信し、予想円を地図上に描画したり、最大震度、到達予想時間などを画面上に表示します。
+
+
+特徴 
+Webブラウザ上で実行可能
+大きく分かりやすい震度表示
+自分好みの配色に設定可能
+到達予想時間分かる
+リプレイ機能搭載
+
+お借りしているAPI等 
+強震モニタ by Wolf20482様 - 緊急地震速報を受信するため
+Yahoo地震情報 by Yahoo様 - 予報円を受信するため
+WorldTimeAPI - 時刻を取得するため
+
+技術仕様 
+URLに「?replay_daytime=20220709163752」などと、年月日時分秒の順に14桁の数字を入れると、その時間からリプレイを開始します。（例では、2022年07月09日16時37分52秒からリプレイが開始されます。）
+
+
+
+ 
+地震監視界隈
+Top
+地震監視者
+地震観測者
+地震情報提供者
+地震情報共有団体
+地震監視配信団体
+地震監視とは
+地震観測とは
+情報提供とは
+観測と監視の違い
+監視ソフト一覧
+地震ライブネットワーク
+地震観測点情報
+
+最新の20件
+2022-07-12
+地震観測者
+関東地方
+九州・沖縄地方
+中部地方
+東北地方
+近畿地方
+2022-07-09
+MiyakojimaCam
+RealtimeQuakeInfo
+RecentCreated
+監視ソフト一覧
+2022-07-02
+KagoshimaCam
+2022-07-01
+編集希望まとめ
+2022-06-25
+JSnetwork
+2022-06-19
+EWRS共有局
+近くの畑のおネギ
+KuriharaCam
+FrontPage
+日本語
+RecentDeleted
+MenuBar
+〔人気/今日人気〕〔最新〕
+T.2　Y.2
+NOW.2　TOTAL.18
+地震ライブネットワーク　EWRS共有局が管理しております。　荒らしは犯罪です。おやめください。
+
